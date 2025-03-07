d51f7f28b9ac4cccc2482bebf173ea0c6a551167
Navid Hasan <navid.nyon@gmail.com>
2025-03-08 05:03:31 +0600

    dotenv move to top

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const { ObjectId } = require("mongodb");
